a8c2f4a8f8daf59c0a99d0d32c56e9b298cec293
skxu <skx@berkeley.edu>
2014-02-28 15:46:30 -0800

    setting up skeleton for png.c

--- png.c
+++ png.c
@@ -10,11 +10,19 @@
  * If it isn't a PNG file, return -1 and print nothing.
  */
 int analyze_png(FILE *f) {
+	
+
     /* YOU WRITE THIS PART */
 	//Check if first 8 bytes match standard png type
 	unsigned int i = 0;
+	
 	unsigned char png_check[8];
-	unsigned char png_format[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
+	const unsigned char png_format[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
+	const unsigned char text_format[4] = {0x74, 0x45, 0x58, 0x74};
+	const unsigned char ztxt_format[4] = {0x7A, 0x54, 0x58, 0x74};
+	const unsigned char time_format[4] = {0x74, 0x49, 0x4D, 0x45};
+	const unsigned char end_format[4] = {0x49, 0x45, 0x4e, 0x44};
+
 	for (i=0; i<8; i++) {
 		/*
 		size_t fread ( void * ptr, size_t size, size_t count, FILE * stream );
@@ -39,17 +47,47 @@ int analyze_png(FILE *f) {
 	}
 
 	//moving on to chunks
+	//The following code below should be in a loop, check chunks 1-by-1 until done
+
 	unsigned int length = 0;
 	unsigned char chunktype[4];
 	fread(&length, 4, 1, f);
 	for (i=0; i<4; i++) {
 		fread(&chunktype[i],1,1,f);
-		printf("%x\n", chunktype[i]);
+		printf("%c\n", chunktype[i]);
 	}
 	printf("\n%d\n", length);
-	//printf("%x", (const char *) png_check);
-	//char file_name[]
 
-    //printf(file_name)
+	if (checkChunk(chunktype, text_format) == true) { // DO tEXt stuff here
+		printf("%s", "tEXt chunktype\n");
+
+	} else if (checkChunk(chunktype, ztxt_format) == true) { //DO zTXt stuff here
+		printf("%s", "zTXt chunktype\n");
+
+	} else if (checkChunk(chunktype, time_format) == true) { //DO tIME stuff here
+		//there should only be ONE tIME chunk in a valid PNG file
+		printf("%s", "tIME chunktype\n");
+
+	} else if (checkChunk(chunktype, end_format) == true) { //ENDING stuff here
+		printf("%s", "IEND chunktype\n");
+
+	} else { //this chunk is irrelevant, pass it and get to the next chunk
+		printf("%s", "irrelevant chunktype\n");
+
+	}
+
+
+	
+
     return -1;
 }
+
+bool checkChunk(unsigned char *test_chunk, const unsigned char *format) {
+	int i = 0;
+		for (i = 0; i<4; i++) {
+			if (test_chunk[i] != format[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
--- png.h
+++ png.h
@@ -2,5 +2,10 @@
 #define PNG_H_GUARD
 
 int analyze_png(FILE *f);
+typedef enum {
+	false = 0,
+	true
+} bool;
+bool checkChunk(unsigned char *test_chunk, const unsigned char *format );
 
 #endif
