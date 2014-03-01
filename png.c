5f57715b98a14ceb570627a909482ae5e392b642
skxu <skx@berkeley.edu>
2014-02-28 16:07:45 -0800

    did stuff

--- png.c
+++ png.c
@@ -15,7 +15,7 @@ int analyze_png(FILE *f) {
     /* YOU WRITE THIS PART */
 	//Check if first 8 bytes match standard png type
 	unsigned int i = 0;
-	
+	bool done = false;
 	unsigned char png_check[8];
 	const unsigned char png_format[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
 	const unsigned char text_format[4] = {0x74, 0x45, 0x58, 0x74};
@@ -48,32 +48,67 @@ int analyze_png(FILE *f) {
 
 	//moving on to chunks
 	//The following code below should be in a loop, check chunks 1-by-1 until done
+	while (done == false) {
+		unsigned int length = 0;
+		unsigned char chunktype[4];
+		unsigned char checksum[4];
+		fread(&length, 4, 1, f); //read the chunk length (big endian int)
 
-	unsigned int length = 0;
-	unsigned char chunktype[4];
-	fread(&length, 4, 1, f);
-	for (i=0; i<4; i++) {
-		fread(&chunktype[i],1,1,f);
-		printf("%c\n", chunktype[i]);
-	}
-	printf("\n%d\n", length);
+		printf("%s", "The chunktype is: ");
+		for (i=0; i<4; i++) { 	//read the chunktype (ASCII)
+			fread(&chunktype[i],1,1,f);
+			printf("%c", chunktype[i]);
+		}
 
-	if (checkChunk(chunktype, text_format) == true) { // DO tEXt stuff here
-		printf("%s", "tEXt chunktype\n");
+		printf("\nThe length of this chunk's data is: %d\n", length);
 
-	} else if (checkChunk(chunktype, ztxt_format) == true) { //DO zTXt stuff here
-		printf("%s", "zTXt chunktype\n");
+		if (checkChunk(chunktype, text_format) == true) { // DO tEXt stuff here
+			printf("%s", "tEXt chunktype\n");
+			
+			//filler method for now, NEED TO CHANGE
+			unsigned char junk[length];
+			for (i=0; i<length; i++) {
+				fread(&junk,1,1,f);
+			}
 
-	} else if (checkChunk(chunktype, time_format) == true) { //DO tIME stuff here
-		//there should only be ONE tIME chunk in a valid PNG file
-		printf("%s", "tIME chunktype\n");
+		} else if (checkChunk(chunktype, ztxt_format) == true) { //DO zTXt stuff here
+			printf("%s", "zTXt chunktype\n");
+			
+			//filler method for now, NEED TO CHANGE
+			unsigned char junk[length];
+			for (i=0; i<length; i++) {
+				fread(&junk,1,1,f);
+			}
 
-	} else if (checkChunk(chunktype, end_format) == true) { //ENDING stuff here
-		printf("%s", "IEND chunktype\n");
+		} else if (checkChunk(chunktype, time_format) == true) { //DO tIME stuff here
+			//there should only be ONE tIME chunk in a valid PNG file
+			printf("%s", "tIME chunktype\n");
+			
+			//filler method for now, NEED TO CHANGE
+			unsigned char junk[length];
+			for (i=0; i<length; i++) {
+				fread(&junk,1,1,f);
+			}
 
-	} else { //this chunk is irrelevant, pass it and get to the next chunk
-		printf("%s", "irrelevant chunktype\n");
+		} else if (checkChunk(chunktype, end_format) == true) { //ENDING stuff here
+			printf("%s", "IEND chunktype\n");
+			done = true;
+		} else { //this chunk is irrelevant, pass it and get to the next chunk
+			printf("%s", "irrelevant chunktype\n");
 
+			//NOT SURE IF THIS IS DANGEROUS OR NOT HELP ME
+			//lack of sleep atm
+			//pretty sure there's a way to exploit this
+			unsigned char junk[length];
+			for (i=0; i<length; i++) {
+				fread(&junk,1,1,f);
+			}
+
+		}
+
+		for (i=0; i<4; i++) { //read the checksum (CRC-32???)
+			fread(&checksum[i],1,1,f);
+		}
 	}
 
 
